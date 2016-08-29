efe0ec90ae6f3aa6c4b2132b6f67c2e66a5e4c16
falawai16@gmail.com <falawai16@gmail.com>
2016-08-29 09:45:13 +0300

    Add Program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace School {
+    public static class Program {
+        public static void Main() {
+
+            Console.WriteLine("Hello World");
+
+            Console.ReadLine();
+        }
+    }
+
+}
